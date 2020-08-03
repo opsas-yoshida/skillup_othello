e55b8007ae3027e1ac65dee1ca3459921a7f1a55
denden <mato_yoshida@yahoo.co.jp>
2020-08-03 11:31:30 +0900

    test_update

---
+++ headers/sub.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void sub(void)
+{
+    printf("sub.h\n");
+
+    return;
+}
---
+++ hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include ".\headers\sub.h"
+
+int main(void)
+{
+    printf("hello, world\n");
+    sub();
+    return 0;
+}
